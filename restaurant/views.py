7565046638772b5497814964cd3f540ebfc3eb52
Shrish KS <shrishdeivam@gmail.com>
2023-10-19 22:44:20 +0530

    Added serializers html

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework_xml.renderers import XMLRenderer
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view, renderer_classes
 
-@renderer_classes([JSONRenderer])
+@renderer_classes([TemplateHTMLRenderer])
 class MenuItemsView(generics.ListCreateAPIView):
     queryset=MenuItem.objects.all()
     serializer_class=MenuItemSerializer
 
-@renderer_classes([JSONRenderer])
+@renderer_classes([TemplateHTMLRenderer])
 class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
     queryset=MenuItem.objects.all()
     serializer_class=MenuItemSerializer
